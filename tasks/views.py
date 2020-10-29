c47710485358520b4b67bfbe0ad8d029c2bd3608
Gagan <gagandureja675@gmail.com>
2020-10-29 17:09:27 +0530

    added update Day 2

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 
 from .models import *
+
+from .forms import *
 
 # Create your views here.
 
@@ -12,6 +14,28 @@ from .models import *
 def index(request):
 	tasks = Task.objects.all()
 
+	form = TaskForm()
 
-	context = {'tasks':tasks}
+	if request.method == 'POST':
+		form = TaskForm(request.POST)
+		if form.is_valid():
+			form.save()
+		return redirect('/')
+
+	context = {'tasks':tasks, 'form':form}
 	return render(request, 'tasks/list.html', context)
+
+
+def updateTask(request, pk):
+	task = Task.objects.get(id=pk)
+
+	form = TaskForm(instance=task)
+
+	if request.method=='POST':
+		form = TaskForm(request.POST, instance=task)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+
+	context = {'form':form}
+	return render(request, 'tasks/update_task.html', context)
